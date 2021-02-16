de3b1794c958496b7cc56804202c64000022a26f
sherry-xi <13824461468@163.com>
2021-02-16 22:59:08 +0800

    update

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -28,11 +28,11 @@ class IndexController extends BaseController{
         ];
 
         //新闻动态
-        $news = MS("article")->field('id,cid,title,content,thumb')->where(['cid'=>199,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(9)->select();
+        $news = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>199,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(9)->select();
         //通知公告
-        $infomation = MS("article")->field('id,cid,title,content,thumb')->where(['cid'=>200,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
+        $infomation = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>200,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
         //康体服务
-        $service = MS("article")->field('id,cid,title,content,thumb')->where(['cid'=>201,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
+        $service = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>201,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
 
         $channelList = MS("channel")->where(['parent_id'=>['gt',0]])->field('id,parent_id')->select();
 
