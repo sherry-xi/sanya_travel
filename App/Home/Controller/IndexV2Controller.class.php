916c90e186e758be1dc860898a27811fe0b27950
sherry-xi <13824461468@163.com>
2021-02-21 19:02:44 +0800

    update

---
+++ App/Home/Controller/IndexV2Controller.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 网站首页 V2版本 目前测试用 20210221
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+
+class IndexV2Controller extends BaseController{
+
+    /**
+     * 初始化
+     */
+    public function __initialize(){
+
+    }
+    /**
+     * 首页视图
+     */
+    public function index(){
+
+        $this->assign('slide',MS('slide')->where(['status'=>0,'show'=>0])->order("sort asc,id desc")->select());
+
+        $indexArticleChannel = [
+            'news' => 199,
+            'infomation' => 200,
+            'service' => 201,
+        ];
+
+        //新闻动态
+        $news = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>199,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(9)->select();
+        //通知公告
+        $infomation = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>200,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
+        //康体服务
+        $service = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>201,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(8)->select();
+
+        $channelList = MS("channel")->where(['parent_id'=>['gt',0]])->field('id,parent_id')->select();
+
+        //专业介绍
+        $major = MS("article")->field('id,cid,title,content,thumb,create_time')->where(['cid'=>168,'is_del'=>0,'audit'=>1])->order("top desc,id desc")->limit(9)->select();
+
+
+        $this->assign('news',$news);
+        $this->assign('information',$infomation);
+        $this->assign('service',$this->setArticleThumb($service));
+        $this->assign('newsImage',$this->setArticleThumb($news));
+        $this->assign('informationImage',$this->setArticleThumb($infomation));
+
+        $this->assign('channelList',array_column($channelList,null,'id'));;
+
+        $this->assign("channel",array_slice($this->channel,0,6));
+        $this->assign("major",$major);
+        $this->display();
+
+    }
+
+    private function setArticleThumb($articles){
+        $data = [];
+        foreach($articles as $k=>$v){
+            $v = getThumbImage($v);
+            if($v['thumb']){
+                $data[] = $v;
+            }
+        }
+        return $data;
+    }
+
+
+}
---
+++ Public/V2/index/common.js
@@ -0,0 +1,82 @@
+//颜色选择器
+$(function(){
+
+    //手机端特殊处理一些样式
+    if(IsMobile()){
+       // $("#main").css("marginTop","20px");
+
+        $("#footer-quikLink").hide();
+        $("#footer-idea").hide();
+        $("#footer-contract").css("text-align","center");
+        $("#footer-weixin").css("text-align","center");
+
+        $(".index-news").css("margin-top","15px");
+        $(".slider-main").css("padding-top",$("#header").height()-20);
+
+    }else{
+        //$("#main").css("marginTop","65px");
+        //$("#header").css("min-width","1300px");
+        $(".slider-main").css("padding-top",$("#header").height()-15);
+
+        if(window.screen.width > 1440){
+            $(".carousel-item").css("min-height",window.screen.width/3.415);//大分辨率电脑banner不能定400 否则页面看不完整图片
+        }
+    }
+
+    $(document).scroll(function(e){
+        if(IsMobile()){
+            return ;
+        }
+
+        var top = $(document).scrollTop();
+        var number = 700;
+        if(top > number){
+            $("#searchForm").hide();
+            $(".logo-pc").hide(1000);
+        }else{
+            $("#searchForm").fadeIn(500);
+            $(".logo-pc").fadeIn(1000);
+        }
+    });
+
+    if(!IsMobile()){
+        /*
+        var top = $(document).scrollTop();
+        var number = 400;
+        if(top > number){
+            $("#header.header-scrolled").css("padding","0px");
+            $("#searchForm").hide();
+            $(".logo-pc").hide();
+        }else{
+            $("#header.header-scrolled").css("padding","10px 0px");
+            $("#searchForm").show();
+            $(".logo-pc").show();
+        }*/
+    }
+
+    //横幅朱家 鼠标小手指
+    $(".slider-main .carousel-item").each(function(){
+        if($(this).attr('data-url')){
+            $(this).css("cursor","pointer");
+        }
+    });
+    //点击图片打开新页面
+    $(".carousel-inner .carousel-item").click(function(){
+        if($(this).attr('data-url')){
+            var tempwindow=window.open('_blank');
+            tempwindow.location= $(this).attr('data-url');
+        }
+
+    });
+
+
+    //导航栏 banner设置
+    if(IsMobile()){
+        $(".channel-banner").css("margin-top","40px");
+        $("#main").css("margin-top","55px");
+    }else{
+        $(".channel-banner").css("margin-top",$("#header").height()-15);
+    }
+
+});
+
